a0116e6f2c80838b77b1727fc060488c0929e523
prajwal <kondubhatlav.eprajwal@gmail.com>
2019-04-26 12:06:46 -0700

    Added Entity for Organization

---
+++ backend/src/main/java/edu/sjsu/cmpe275/project/Entity/Organization.java
@@ -0,0 +1,63 @@
+package edu.sjsu.cmpe275.project.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ORGANIZATION")
+public class Organization {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private int id;
+	
+	@Column(name = "NAME")
+	private String name;
+	
+	@Embedded
+	private Address address;
+	
+	@Column(name = "DESCRIPTION")
+	private String description;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+}
