c113cd544157ea01284635af0da0aac67a741769
Bayamba <Bayamba@10.250.237.163>
2019-04-27 01:38:08 -0700

    Created service for hackathon and organization

---
+++ backend/src/main/java/edu/sjsu/cmpe275/project/Service/HackathonService.java
@@ -0,0 +1,29 @@
+package edu.sjsu.cmpe275.project.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.sjsu.cmpe275.project.Entity.Hackathon;
+import edu.sjsu.cmpe275.project.Repository.HackathonDao;
+
+@Service
+@Transactional
+public class HackathonService {
+	@Autowired
+	HackathonDao hackathonDao;
+	
+	public List<Hackathon> getAllHackathons() {
+        return this.hackathonDao.findAll();
+    }
+	
+	public Hackathon addHackathon(Hackathon hackathon) {
+        return this.hackathonDao.save(hackathon);
+    }
+	
+	public void deleteHackathon(int ID) {
+        this.hackathonDao.deleteById(ID);
+    }
+}
---
+++ backend/src/main/java/edu/sjsu/cmpe275/project/Service/OrganizationService.java
@@ -0,0 +1,29 @@
+package edu.sjsu.cmpe275.project.Service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.sjsu.cmpe275.project.Entity.Organization;
+import edu.sjsu.cmpe275.project.Repository.OrganizationDao;
+
+@Service
+@Transactional
+public class OrganizationService {
+	@Autowired
+	OrganizationDao organizationDao;
+	
+	public List<Organization> getAllOrganizations() {
+        return this.organizationDao.findAll();
+    }
+	
+	public Organization addOrganization(Organization organization) {
+        return this.organizationDao.save(organization);
+    }
+	
+	public void deleteOrganization(int ID) {
+        this.organizationDao.deleteById(ID);
+    }
+}
