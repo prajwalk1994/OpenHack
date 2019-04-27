72b67d34c7236459971d18c1a967f8479a4b4e7d
prajwal <kondubhatlav.eprajwal@gmail.com>
2019-04-27 01:40:45 -0700

    Added Service for User

---
+++ backend/src/main/java/edu/sjsu/cmpe275/project/Service/UserService.java
@@ -0,0 +1,30 @@
+package edu.sjsu.cmpe275.project.Service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.sjsu.cmpe275.project.Entity.User;
+import edu.sjsu.cmpe275.project.Repository.UserDao;
+
+@Service
+@Transactional
+public class UserService {
+
+	@Autowired
+	UserDao userDao;
+
+	public User addUser(User user) {
+		return this.userDao.save(user);
+	}
+
+	public Optional<User> getUser(int id) {
+		return this.userDao.findById(id);
+	}					
+
+	public void deleteUser(int id) {
+		this.userDao.deleteById(id);
+	}
+}
