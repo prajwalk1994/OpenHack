d76b0b23fa9701621bc66f518602e69908756533
vinay <vinay@vinays-mbp.attlocal.net>
2019-04-27 02:53:37 -0700

    Added Hackathon_Teams entity module

--- backend/src/main/java/edu/sjsu/cmpe275/project/Entity/Hackathon.java
+++ backend/src/main/java/edu/sjsu/cmpe275/project/Entity/Hackathon.java
@@ -3,6 +3,7 @@ package edu.sjsu.cmpe275.project.Entity;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.JoinColumn;;
 
@@ -63,5 +65,112 @@ public class Hackathon {
 
 	@Column(name = "STATUS")
 	private String status;
+	
+	@OneToMany(mappedBy = "hackId", cascade = CascadeType.ALL)
+    private List<HackathonTeams> HackathonTeams;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<User> getJudgeList() {
+		return judgeList;
+	}
+
+	public void setJudgeList(List<User> judgeList) {
+		this.judgeList = judgeList;
+	}
+
+	public List<Organization> getOrgList() {
+		return orgList;
+	}
+
+	public void setOrgList(List<Organization> orgList) {
+		this.orgList = orgList;
+	}
+
+	public int getRegFee() {
+		return regFee;
+	}
+
+	public void setRegFee(int regFee) {
+		this.regFee = regFee;
+	}
+
+	public int getMinTeam() {
+		return minTeam;
+	}
+
+	public void setMinTeam(int minTeam) {
+		this.minTeam = minTeam;
+	}
+
+	public int getMaxTeam() {
+		return maxTeam;
+	}
+
+	public void setMaxTeam(int maxTeam) {
+		this.maxTeam = maxTeam;
+	}
+
+	public int getSponDiscount() {
+		return sponDiscount;
+	}
+
+	public void setSponDiscount(int sponDiscount) {
+		this.sponDiscount = sponDiscount;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public List<HackathonTeams> getHackathonTeams() {
+		return HackathonTeams;
+	}
+
+	public void setHackathonTeams(List<HackathonTeams> hackathonTeams) {
+		HackathonTeams = hackathonTeams;
+	}
 
 }
--- backend/src/main/java/edu/sjsu/cmpe275/project/Entity/Team.java
+++ backend/src/main/java/edu/sjsu/cmpe275/project/Entity/Team.java
@@ -1,5 +1,8 @@
 package edu.sjsu.cmpe275.project.Entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -8,6 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -71,7 +75,10 @@ public class Team {
 	public void setRole(Role role) {
 		this.role = role;
 	}
-	
+	@OneToMany
+//	(mappedBy = "hackId", cascade = CascadeType.ALL)
+	@JoinColumn(name = "teamId",table="HackathonTeams")
+    private List<HackathonTeams> HackathonTeams;		
 	
 	
 }
