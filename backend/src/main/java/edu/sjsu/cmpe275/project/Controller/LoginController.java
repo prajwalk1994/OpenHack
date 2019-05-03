0765f688faa84c581656d368c96925d3141141c0
kovurivinay <kovurivinay19@gmail.com>
2019-05-03 15:51:45 -0700

    Added Login controller

---
+++ backend/src/main/java/edu/sjsu/cmpe275/project/Controller/LoginController.java
@@ -0,0 +1,37 @@
+package edu.sjsu.cmpe275.project.Controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.sjsu.cmpe275.project.Entity.User;
+import edu.sjsu.cmpe275.project.Service.UserService;
+
+@RestController
+@RequestMapping("/")
+public class LoginController {
+	
+	@Autowired 
+	UserService userService;
+	
+	@PostMapping("/login")
+	public ResponseEntity<Object> loginUser(@RequestBody User user) {
+		Optional<User> loginUser = userService.getUser(user.getId());
+		if(!loginUser.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		if(loginUser.get().getPassword()==user.getPassword()) {
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+//	@PostMapping("/signUp"){
+//		
+//	}
+}
