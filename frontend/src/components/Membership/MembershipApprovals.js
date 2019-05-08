6901fa63c59b305e86ff1633c01a424684ae059d
prajwal <kondubhatlav.eprajwal@gmail.com>
2019-05-08 16:16:38 -0700

    Membership approval component

--- frontend/src/components/Membership/MembershipApprovals.js
+++ frontend/src/components/Membership/MembershipApprovals.js
@@ -3,61 +3,93 @@ import Axios from 'axios';
 import url from '../../config/config';
 
 class MembershipApprovals extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            userid:localStorage.getItem("userid"),
-            username:["ranjith","prajwal","vinay"],
-            orgName:["sjsu","isss","isis"],
-            status:[]
+        this.state = {
+            userid: localStorage.getItem("userid"),
+            username: [],
+            orgName: [],
+            userids: [],
+            status: [],
+            orgids: [],
+            disableButton: false
         }
     }
 
-    componentDidMount=()=>{
-        Axios.get()
-    }
+    componentDidMount = () => {
+        // e.preventDefault();
+        Axios.get(url + "/organizationRequests/" + this.state.userid)
+            .then((response) => {
+                var tempusername = []
+                var temporgName = []
+                var tempstatus = []
+                var tempuserids = []
+                var temporgids = []
+                for (let i of response.data) {
+                    tempuserids.push(i.user.id);
+                    tempusername.push(i.user.username);
+                    temporgName.push(i.organization.name);
+                    temporgids.push(i.organization.id);
+                    if (i.approval == "No") {
+                        tempstatus.push("Approve")
+                    } else {
+                        tempstatus.push("Approved")
+                        if (i.approval == "Yes") {
+                            this.setState({
+                                disableButton: true,
+                            })
+                        } else {
+                            this.setState({
+                                disableButton: false,
+                            })
+                        }
 
-    handleApprove = (e)=>{
-        e.preventDefault();
+                    }
+                }
 
-    }
-
-    handleApprove=(e)=>{
-        e.preventDefault();
-        Axios.get(url+"/organizationRequests/"+this.state.userid)
-        .then((response)=>{
-            var tempusername = []
-            var temporgName = []
-            var tempstatus =[]
-            for(let i of response.data){
-                tempusername.push(i.user.username);
-                temporgName.push(i.organization.name);
-                tempstatus.push(i.approval)
-            }
-
-            this.setState({
-                username:tempusername,
-                orgName:temporgName,
-                status:tempstatus
+                this.setState({
+                    userids: tempuserids,
+                    username: tempusername,
+                    orgName: temporgName,
+                    orgids: temporgids,
+                    status: tempstatus
+                })
             })
-        })
-        .catch((error)=>{
-            if(error.response){
-                console.log(error.response.status)
-            }else{
-                alert("something went wrong")
-            }
-        })
+            .catch((error) => {
+                if (error.response) {
+                    console.log(error.response.status)
+                } else {
+                    alert("something went wrong")
+                }
+            })
+    }
+
+
+
+    handleApprove = (e, userId, orgId) => {
+        e.preventDefault();
+        Axios.post(url + "/activateMember/" + userId + "/" + this.state.userid + "/" + orgId)
+            .then((response) => {
+                console.log(response.data);
+                alert("Membership confirmed");
+            })
+            .catch((error) => {
+                if (error.response) {
+                    console.log(error.response.status)
+                } else {
+                    alert("something went wrong")
+                }
+            })
     }
 
     render() {
         var MembershipRequests = "";
-        MembershipRequests = this.state.username.map((item,i)=>{
-            return(
+        MembershipRequests = this.state.username.map((item, i) => {
+            return (
                 <div className="row justify-content-center">
                     <label className="col-sm-3">{item}</label>
                     <label className="col-sm-3">{this.state.orgName[i]}</label>
-                    <button className="btn btn-success mr-2" onClick={this.handleApprove}>{this.state.status[i]}</button>
+                    <button className="btn btn-success mr-2" disabled={(this.state.status[i] === "Approved") ? true : false} onClick={(e) => this.handleApprove(e, this.state.userids[i], this.state.orgids[i])} >{this.state.status[i]}</button>
                     {/* <button className="btn btn-danger" onClick={this.handleDeny}>Deny</button> */}
                 </div>
             )
@@ -65,7 +97,7 @@ class MembershipApprovals extends Component {
 
         return (
             <div className="container">
-            <h3>Membership Approvals</h3>
+                <h3>Membership Approvals</h3>
                 <div className="row justify-content-center">
                     <h6 className="col-sm-3">Username</h6>
                     <h6 className="col-sm-3">Organization name</h6>
