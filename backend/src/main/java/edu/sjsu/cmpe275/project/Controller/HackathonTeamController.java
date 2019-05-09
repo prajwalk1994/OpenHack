d347ed338a065386402b194b02f391a2008ba613
prajwal <kondubhatlav.eprajwal@gmail.com>
2019-05-08 17:08:23 -0700

    Added hackathons by User component

--- backend/src/main/java/edu/sjsu/cmpe275/project/Controller/HackathonTeamController.java
+++ backend/src/main/java/edu/sjsu/cmpe275/project/Controller/HackathonTeamController.java
@@ -1,5 +1,6 @@
 package edu.sjsu.cmpe275.project.Controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +21,12 @@ import edu.sjsu.cmpe275.project.Entity.Hackathon;
 import edu.sjsu.cmpe275.project.Entity.HackathonTeams;
 import edu.sjsu.cmpe275.project.Entity.Team;
 import edu.sjsu.cmpe275.project.Entity.TeamMember;
+import edu.sjsu.cmpe275.project.Entity.User;
 import edu.sjsu.cmpe275.project.Service.HackathonService;
 import edu.sjsu.cmpe275.project.Service.HackathonTeamsService;
 import edu.sjsu.cmpe275.project.Service.TeamMemberService;
 import edu.sjsu.cmpe275.project.Service.TeamService;
+import edu.sjsu.cmpe275.project.Service.UserService;
 
 @RestController
 @RequestMapping("/")
@@ -39,6 +43,9 @@ public class HackathonTeamController {
 	
 	@Autowired
 	TeamMemberService teamMemberService;
+	
+	@Autowired
+	UserService userService;
 	
 	@PostMapping("/hackathonteam/{hackId}/{teamId}")
 	public ResponseEntity<Object> addTeamToHackathon(@PathVariable("hackId") int hackId, @PathVariable("teamId") int teamId){
@@ -71,4 +78,29 @@ public class HackathonTeamController {
 		return null;
 	}
 	
+	
+	@GetMapping("/hackathonsByUser/{userId}")
+	public ResponseEntity<Object> getHackathonsByUser(@PathVariable("userId") int userId){
+		try {
+			Optional<User> user = this.userService.getUser(userId);
+			List<Hackathon> hackathons = new ArrayList<>();
+			if(!user.isPresent()) {
+				return new ResponseEntity<Object>("User not found", HttpStatus.NOT_FOUND);
+			}
+			List<TeamMember> teams = this.teamMemberService.getTeamMemberByUser(userId);
+			for(TeamMember member : teams) {
+				Team team = member.getTeam();
+				List<HackathonTeams> hackathonTeams = this.hackathonTeamsService.getHackathonTeamsByTeamId(team.getId());
+				System.out.println(hackathonTeams);
+				for(HackathonTeams hackTeam: hackathonTeams) {
+					hackathons.add(hackTeam.getHackId());
+				}
+			}
+			return new ResponseEntity<Object>(hackathons, HttpStatus.OK);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<Object>("Bad Request", HttpStatus.BAD_REQUEST);
+		}
+	}
 }
