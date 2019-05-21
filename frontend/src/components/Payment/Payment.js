add1dd57b40ac9f3e1897ec6b66e206efcd38341
kovurivinay <kovurivinay19@gmail.com>
2019-05-21 00:28:37 -0700

    Team payments and Email Confirmations done

--- frontend/src/components/Payment/Payment.js
+++ frontend/src/components/Payment/Payment.js
@@ -9,9 +9,11 @@ class Payment extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            email:localStorage.getItem("email"),
             userid:"2",
             tempHackId:"",
             regFee:"",
+            teamid:"",
             price: 0,
             discount: 0,
             redirectVar: ""
@@ -34,10 +36,12 @@ class Payment extends Component {
                 for (let i=0;i<hackathons.length;i++){
                     if(hackathons[i].hackId.id==this.state.hackId){
                         this.setState({
-                            regFee:hackathons[i].hackId.regFee
+                            regFee:hackathons[i].hackId.regFee,
+                            // tempId:hackathons[i].teamId.id
                         })
                     }
                 }
+                console.log(this.state)
                 
             })
             .catch((error) => {
@@ -47,7 +51,7 @@ class Payment extends Component {
             .then((response) => {
                 console.log("response", response.data);
                 this.setState({
-                    discount:response.data
+                    discount:response.data || 0
                 })
             })
             .catch((err) => {
@@ -58,7 +62,18 @@ class Payment extends Component {
 
     makePayment = (e) => {
         e.preventDefault();
-        alert("Payment email sent!")
+        
+        Axios.post(url + `/makePayment/${this.state.userid}/${this.state.teamid}/${this.state.email}/${this.state.regFee*(1-this.state.discount/100)}`)
+            .then((response) => {
+                console.log("response", response.data);
+                alert("Payment Successful")
+            })
+            .catch((err) => {
+                console.log(err);
+                // alert(err.response.data);
+            })
+        // alert("Payment email sent!")
+
 
         // this.setState({
         //     redirectVar: <Redirect to="/submission"></Redirect>
@@ -91,7 +106,7 @@ class Payment extends Component {
                             </div>
                             <div className="row justify-content-center">
                                 <label className="col-sm-2" >Total</label>
-                                <input className="form-control col-sm-4" type="text" name="discount" value={this.state.regFee*(1-this.state.discount/100)} disabled></input>
+                                <input className="form-control col-sm-4" type="text" name="finalValue" value={this.state.regFee*(1-this.state.discount/100)} disabled></input>
                             </div>
                             <div className="row justify-content-right">
                                 <label for="cardNumber" className="col-sm-4">CARD NUMBER</label>
